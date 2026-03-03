038576ef44b09f950200ff2a372d99bae9230933
Noé Conti <noeconti34@gmail.com>
2026-03-03 19:09:15 +0100

    L'agent tourne indéfiniement toutes les 3 secondes

--- agent/agent.py
+++ agent/agent.py
@@ -2,14 +2,15 @@ import psutil
 import time
 import requests
 
+def ramPush():
+    ram = psutil.virtual_memory()
+    url = "http://localhost:8080/metric/ram"
+    myobj = {'ram_pourcentage': ram.percent}
+    x = requests.post(url, json = myobj)
 
-ram = psutil.virtual_memory()
-
-url = "http://localhost:8080/metric/ram"
-
-myobj = {'ram_pourcentage': ram.percent}
-
-x = requests.post(url, json = myobj)
+while True:
+    ramPush()
+    time.sleep(3)
 
 #print(ram.percent)
 # print("CPU usage (%):", psutil.cpu_percent(interval=1))
