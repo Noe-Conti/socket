d7e56b5f0b1dc86243a3f7c1af8fbec4dada39e9
YanraC13 <145334517+YanraC13@users.noreply.github.com>
2026-07-01 19:59:11 +0200

    feat: MongoDB integration + mTLS SSL + dashboard fix

--- run_server.py
+++ run_server.py
@@ -9,5 +9,5 @@ uvicorn.run(
     ssl_keyfile="certs/server.key",
     ssl_certfile="certs/server.crt",
     ssl_ca_certs="certs/ca.crt",
-    ssl_cert_reqs=ssl.CERT_REQUIRED,
+    ssl_cert_reqs=ssl.CERT_OPTIONAL,  # CERT_OPTIONAL : agents envoient leur cert, navigateur peut se connecter sans
 )
