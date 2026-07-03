3c04ae67b41e8346d0fd3f1893ea01e133b51d1b
Noé Conti <noeconti34@gmail.com>
2026-07-03 19:07:09 +0200

    feat: Autorisé les connexions depuis n'importe quelle source

--- main.py
+++ main.py
@@ -54,16 +54,9 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "https://localhost:3000",
-    "https://127.0.0.1:3000"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
